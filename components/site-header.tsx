68f8b781a2099fc42465202ddcaf59af7b4f79f5
stefw <sw@magicmorning.net>
2023-03-05 20:51:07 +0100

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -13,7 +13,6 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-
             <ThemeToggle />
           </nav>
         </div>
